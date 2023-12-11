a6285e7c1f1903cd4cda259b12d72df95b1201db
Emilio Zapata <124ezapata@frhsd.com>
2023-12-11 08:37:19 -0500

    fix it

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+   // alert("jquery loaded")
+   $(h3.css({border:"3px solid blue"}));
+   $(".wrapper").css({border: "3px solid red"});
+$("#clients").css({border:"3px solid yellow"});
+
+$("#contact-website").css((border:"2px solid red")).next().css((border:2px solid green)).clos
+})
